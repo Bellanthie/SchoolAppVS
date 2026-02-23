996f8293b6be99b7ac3e17bf3d750a90e0987f5f
Izabella <bellanthie@gmail.com>
2026-02-23 22:15:19 +0100

    Add project files.

---
+++ SchoolAppVS/Models/Departments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAppVS.Models
+{
+    public class Department // properties to match Id, name, Staff
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Staff> Staff { get; set; }
+
+    }
+}
---
+++ SchoolAppVS/Models/Staff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAppVS.Models
+{
+    public class Staff // properties to match Staff: id, FirstName, Lastname, Title, Salary, StartDate, DepartmentId, Department
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime StartDate { get; set; }
+        public Department Department { get; set; } 
+        // these two together tells EF "this staff member belongs to one department" (like references in SQL)
+
+    }
+}
